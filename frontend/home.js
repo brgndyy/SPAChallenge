a65befb0fd0c8d53f328d06bf6ee929310f00785
brgndy <brgndy96@gmail.com>
2023-01-10 17:00:16 +0900

    msg

--- frontend/home.js
+++ frontend/home.js
@@ -1,6 +1,6 @@
 // 아이디 셀렉터
 
-const post_ul = document.getElementById("post-list");
+const post_ul = document.getElementById("post_list");
 
 const IMGURL = `https://api.unsplash.com/photos/?client_id=up6CzpiP44kToAJe1U1FdqFRfHLxXzBIR2A9Yc_N7zc`;
 
@@ -9,14 +9,25 @@ const getPosts = async () => {
   axios
     .get("http://43.201.103.199/posts")
     .then((res) => {
-      console.log(res.data.data);
+      const posts = res.data.data.posts;
+
+      post_ul.innerHTML = posts
+        .map((post) => {
+          return `<li id="post" class="post">
+        <a href="">
+          <img
+            src=${post.image}
+            alt="thumnailImg"
+          />
+          <div>
+            <strong>${post.title}</strong>
+            <p>${post.content}</p>
+          </div>
+        </a>
+      </li>`;
+        })
+        .join("");
     })
     .catch((error) => console.error(error));
 };
 getPosts();
-
-// const li = document.createElement('li');
-// li.classList.add('post');
-// const a = document.createElement('a');
-
-// li.append(a);
