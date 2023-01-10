25473bd25326ee3f7c9f0a57083eeb9eab036179
brgndy <brgndy96@gmail.com>
2023-01-10 15:27:22 +0900

    msg

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+// 정적파일 제공
+app.use("/", express.static(path.resolve(__dirname + "/frontend")));
+
+// 메인페이지로 접근시, home.html 렌더링
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/frontend/home.html"));
+});
+
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
 });
